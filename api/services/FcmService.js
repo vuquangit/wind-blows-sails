17dcd901965191635604fe2980f02d056e428fc1
Quang <vuquangit@gmail.com>
2020-02-11 22:06:40 +0700

    fix: fix env

--- api/services/FcmService.js
+++ api/services/FcmService.js
@@ -1,13 +1,21 @@
-const serviceAccount = require("./service-account.json");
 const admin = require("firebase-admin");
-// const axios = require("axios");
-// const dotenv = require("dotenv");
-// dotenv.config();
-// const WEB_PUSH_CERTIFICATES = process.env.GCM_WEB_PUSH_CERTIFICATES;
+
+const defaultAppConfig = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.envFIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 /* initialise app */
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(defaultAppConfig)
 });
 
 /* initialise firestore */
