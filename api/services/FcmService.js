d3ed6edcb45477f01e354ad70e4d61598c641e43
Quang <vuquangit@gmail.com>
2020-02-12 22:44:04 +0700

    feat: modify firebase init

--- api/services/FcmService.js
+++ api/services/FcmService.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./service-account.json");
+// const serviceAccount = require("./service-account.json");
 
 /* initialise app */
 // const firebaseConfig = {
@@ -13,6 +13,19 @@ const serviceAccount = require("./service-account.json");
 //   token_uri: "https://oauth2.googleapis.com/token"
 // };
 // admin.initializeApp(firebaseConfig);
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URL,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
